6756fb78f9cf87d527638f7bc66d40a0ef472b89
Logan Smith <logsmith06@gmail.com>
2020-01-23 04:06:09 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,4 +5,4 @@ end
 
 get '/name' {"My name is Logan."}
 get '/hometown' {"My hometown is Reno, Nevada."}
-get 'favorite-song' {"My favorite song is Big Bad Wolf."}
+get '/favorite-song' {"My favorite song is Big Bad Wolf."}
