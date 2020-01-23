5511da854c9816375e578f7eb4f5b6d75444c29a
Logan Smith <logsmith06@gmail.com>
2020-01-23 04:10:15 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,9 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do "Bing Bong" end
+  get '/name' do "My name is Logan." end
+  get '/hometown' do "My hometown is Reno, Nevada." end
+  get '/favorite-song' do "My favorite song is Big Bad Wolf." end
+
 end
