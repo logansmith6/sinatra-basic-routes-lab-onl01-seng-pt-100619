092103c68b0c7592c040038e7550e8da39ee37a5
Logan Smith <logsmith06@gmail.com>
2020-01-23 04:08:47 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,7 +5,5 @@ class App < Sinatra::Base
   get '/name' do "My name is Logan." end
   get '/hometown' do "My hometown is Reno, Nevada." end
   get '/favorite-song' do "My favorite song is Big Bad Wolf." end
-  
+
 end
-
-
