9c05e465f8cbf16ad2b9b313965740929074089f
Logan Smith <logsmith06@gmail.com>
2020-01-23 04:07:28 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,7 +1,8 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  get '/name' {"My name is Logan."}
-  get '/hometown' {"My hometown is Reno, Nevada."}
-  get '/favorite-song' {"My favorite song is Big Bad Wolf."}
 end
+
+get '/name' {"My name is Logan."}
+get '/hometown' {"My hometown is Reno, Nevada."}
+get '/favorite-song' {"My favorite song is Big Bad Wolf."}
